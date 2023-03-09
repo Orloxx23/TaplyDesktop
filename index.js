86ee0690f9e88e87f6cda4c24e672eb8bd65750b
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-03-08 22:58:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,26 @@ const https = require("https");
 const http = require("https");
 const fetch = require("node-fetch");
 const WebSocket = require("ws");
+const { match } = require("assert");
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const PORT = process.env.PORT || 3000;
+
+let lockData = null;
 
 let token = null;
 let entitlement = null;
 let puuid = null;
 let pid = null;
+let region = null;
+let shard = null;
+let clientVersion = null;
+let partyId = null;
+let party = null;
+let gameModes = null;
+let preGame = null;
+let currentMatch = null;
 
 const localAgent = new https.Agent({
   rejectUnauthorized: false,
@@ -37,6 +48,23 @@ async function getLockfileData() {
   let d = {};
   [d.name, d.pid, d.port, d.password, d.protocol] = contents.split(":");
   return d;
+}
+
+async function waitForLockfile() {
+  return new Promise(async (resolve, reject) => {
+    const watcher = fs.watch(
+      path.join(
+        process.env["LOCALAPPDATA"],
+        "Riot Games\\Riot Client\\Config\\"
+      ),
+      (eventType, fileName) => {
+        if (eventType === "rename" && fileName === "lockfile") {
+          watcher.close();
+          resolve();
+        }
+      }
+    );
+  });
 }
 
 async function getSession(port, password) {
@@ -63,32 +91,20 @@ async function getHelp(port, password) {
   ).json();
 }
 
-async function waitForLockfile() {
-  return new Promise(async (resolve, reject) => {
-    const watcher = fs.watch(
-      path.join(
-        process.env["LOCALAPPDATA"],
-        "Riot Games\\Riot Client\\Config\\"
-      ),
-      (eventType, fileName) => {
-        if (eventType === "rename" && fileName === "lockfile") {
-          watcher.close();
-          resolve();
-        }
-      }
-    );
-  });
-}
-
-async function getEntitlementsToken(port) {
+async function getEntitlementsToken(port, password) {
   let url = `https://127.0.0.1:${port}/entitlements/v1/token`;
+  const username = "riot";
 
   let options = {
     method: "GET",
-    headers: { Authorization: "Basic cmlvdDo5VDRzZ202dEZHVGxfUHJNQTk1RXlR" },
+    headers: {
+      Authorization: `Basic ${Buffer.from(username + ":" + password).toString(
+        "base64"
+      )}`,
+    },
   };
 
-  fetch(url, options)
+  await fetch(url, options)
     .then((res) => res.json())
     .then((json) => {
       entitlement = json.token;
@@ -97,35 +113,244 @@ async function getEntitlementsToken(port) {
     .catch((err) => console.error("error:" + err));
 }
 
-async function getPUUID(port) {
+async function getPUUID(port, password) {
+  console.log("getting puuid");
+
   let url = `https://127.0.0.1:${port}/chat/v1/session`;
+  const username = "riot";
 
   let options = {
     method: "GET",
-    headers: { Authorization: "Basic cmlvdDo5VDRzZ202dEZHVGxfUHJNQTk1RXlR" },
+    headers: {
+      Authorization: `Basic ${Buffer.from(username + ":" + password).toString(
+        "base64"
+      )}`,
+    },
+  };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      puuid = json.puuid;
+      pid = json.pid;
+      if (json.region.includes("la")) {
+        region = "latam";
+        shard = "na";
+      } else if (json.region === "br") {
+        region = json.region;
+        shard = "na";
+      } else {
+        region = json.region;
+        shard = json.region;
+      }
+    })
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getWallet() {
+  let url = `https://pd.na.a.pvp.net/store/v1/wallet/${puuid}`;
+  let wallet = null;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("✅ ~ file: index.js:149 ~ getWallet ~ url:", url);
+      wallet = json;
+    })
+    .catch((err) => console.error("error:" + err));
+
+  return wallet;
+}
+
+async function getVersion() {
+  let url = "https://valorant-api.com/v1/version";
+
+  let options = { method: "GET" };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("✅ ~ file: index.js:169 ~ getVersion ~ url:", url);
+      clientVersion = json.data.riotClientVersion;
+    })
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getPregame() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/pregame/v1/players/${puuid}`;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      "X-Riot-ClientVersion": clientVersion,
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => (preGame = json.MatchID))
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getPartyPlayer() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/players/${puuid}`;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      "X-Riot-ClientVersion": clientVersion,
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("✅ ~ file: index.js:195 ~ getPartyPlayer ~ url:", url);
+      partyId = json.CurrentPartyID;
+    })
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getParty() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/parties/${partyId}`;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      gameModes = json.EligibleQueues;
+      party = json;
+      console.log("✅ ~ file: index.js:225 ~ getParty ~ url:", url);
+    })
+    .catch((err) => console.error("error:" + err));
+}
+
+async function changeQueue(gamemode) {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/parties/${partyId}/queue`;
+
+  let options = {
+    method: "POST",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      "content-type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: `{"queueID":"${gamemode}"}}`,
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) =>
+      console.log("✅ ~ file: index.js:240 ~ changeQueue ~ url:", url)
+    )
+    .catch((err) => console.error("error:" + err));
+}
+
+async function startQueue() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/parties/${partyId}/matchmaking/join`;
+
+  let options = {
+    method: "POST",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      Authorization: "Bearer " + token,
+    },
   };
 
   fetch(url, options)
     .then((res) => res.json())
     .then((json) => {
-      puuid = json.puuid;
-      pid = json.pid;
+      console.log("✅ ~ file: index.js:255 ~ startQueue ~ url:", url);
     })
     .catch((err) => console.error("error:" + err));
 }
 
-async function authCookies() {
-  let url = "https://auth.riotgames.com/api/v1/authorization";
+async function stopQueue() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/parties/${partyId}/matchmaking/leave`;
 
   let options = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      cookie:
-        "__cf_bm=m768G2u1g0LA5ZTW7Gf938PzaQXhEah8JgIUINk503k-1678240756-0-AeRSuqMxeP6zCdziSO3Xdud23c5ebpiIQc2%2FGeF0L8bWynNcx8pswaLXseNk7p3q5Qd9GN9c3BBDcXYj9NBC4rQ%3D; tdid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE1N2JkM2MyLTYzZTgtNGIyNC1iYzgzLTk0N2U3ZDJmYjU2NiIsIm5vbmNlIjoiWEtqU2FuU0FxbDg9IiwiaWF0IjoxNjc4MjQwODk0fQ.jA6anM-Dx0aZXv7rddbiiO96tl1JXdkdhEHcWLCdFBk; asid=GgwXlHTjgHZaC4tf-FqbiUw7KFBgPJjehQQzq_puzd8.3XSHjMMtamQ%253D; clid=ue1; ",
-      accept: "*/*",
+      "X-Riot-Entitlements-JWT": entitlement,
+      Authorization: "Bearer " + token,
     },
-    body: '{"client_id":"play-valorant-web-prod","nonce":"1","redirect_uri":"https://playvalorant.com/opt_in","response_type":"token id_token"}',
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("✅ ~ file: index.js:255 ~ stopQueue ~ url:", url);
+    })
+    .catch((err) => console.error("error:" + err));
+}
+
+async function partyAccessibility(access) {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/parties/v1/parties/${partyId}/accessibility`;
+
+  let options = {
+    method: "POST",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      "content-type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: `{"accessibility":"${access}"}}`,
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) =>
+      console.log("✅ ~ file: index.js:309 ~ partyAccessibility ~ url:", url)
+    )
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getCurrentMatch() {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/core-game/v1/players/${puuid}`;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => (currentMatch = json.MatchID))
+    .catch((err) => console.error("error:" + err));
+}
+
+async function getContracts() {
+  let url = `https://pd.${shard}.a.pvp.net/contracts/v1/contracts/${puuid}`;
+
+  let options = {
+    method: "GET",
+    headers: {
+      "X-Riot-Entitlements-JWT": entitlement,
+      "X-Riot-ClientVersion": clientVersion,
+      Authorization: "Bearer " + token,
+    },
   };
 
   fetch(url, options)
@@ -134,22 +359,22 @@ async function authCookies() {
     .catch((err) => console.error("error:" + err));
 }
 
-async function authRequest() {
-  let url = "https://auth.riotgames.com/api/v1/authorization";
+async function selectAgent(agent) {
+  let url = `https://glz-${region}-1.${shard}.a.pvp.net/pregame/v1/matches/${preGame}/select/${agent}`;
 
   let options = {
-    method: "PUT",
+    method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      cookie:
-        "tdid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE1N2JkM2MyLTYzZTgtNGIyNC1iYzgzLTk0N2U3ZDJmYjU2NiIsIm5vbmNlIjoiWEtqU2FuU0FxbDg9IiwiaWF0IjoxNjc1NjE5NjY3fQ.DzK5ROhq4zrpx2pP1o1jVYZYDd9azOe-r71WZBV99vs; ",
+      "X-Riot-Entitlements-JWT":
+        "eyJraWQiOiJrMSIsImFsZyI6IlJTMjU2In0.eyJlbnRpdGxlbWVudHMiOltdLCJhdF9oYXNoIjoiTGs0dzN6djZYYnZJaGVqemVRejJGZyIsInN1YiI6ImY0ZjliN2NjLTNjMTMtNWU3Mi1iY2UwLTNkYzQ1MDA4MjkwOSIsImlzcyI6Imh0dHBzOlwvXC9lbnRpdGxlbWVudHMuYXV0aC5yaW90Z2FtZXMuY29tIiwiaWF0IjoxNjc4MzMzMjA3LCJqdGkiOiJxU0F3T0dZaHd3NCJ9.fUx2xbMNkBtcyfXFFU7CsydYSlG2TJVCKx9-lCtGVfyQHKez-uBQScP7Z8cx2ynf83XboZK05JPsA4ShpizSsfS07uZlaXXB9NueOVzdTTgoPZruR6OBIbQSpx51SGeR-cM_F2EEu2C8o-LsTcXbhJZktPEia47lixW8c7NTf5u0m643DXyRpocc7z_WM0UT86AKlnf1_Azoqe2FaU0TGhJX08q9EjV8iIH2YNCQ1mKOmSaf7ckTQtB3cqkiHttOaeSS9mh3wWzXR1FQByZBcj67lNPhDH57OP3UZbqbUQwwfyAm8pXXmGZZp0WEjCMNmIaf6d9cct4fNx0luYo26Q",
+      Authorization:
+        "Bearer eyJraWQiOiJzMSIsImFsZyI6IlJTMjU2In0.eyJwcCI6eyJjIjoiYW0ifSwic3ViIjoiZjRmOWI3Y2MtM2MxMy01ZTcyLWJjZTAtM2RjNDUwMDgyOTA5Iiwic2NwIjpbImFjY291bnQiLCJvcGVuaWQiXSwiY2xtIjpbImZlZGVyYXRlZF9pZGVudGl0eV9wcm92aWRlcnMiLCJlbWFpbF92ZXJpZmllZCIsInJnbl9MQTEiLCJvcGVuaWQiLCJwdyIsInBob25lX251bWJlcl92ZXJpZmllZCIsImFjY3RfZ250IiwibG9jYWxlIiwiYWNjdCIsImFnZSIsImFjY291bnRfdmVyaWZpZWQiLCJhZmZpbml0eSJdLCJkYXQiOnsicCI6bnVsbCwiciI6IkxBMSIsImMiOiJ1ZTEiLCJ1IjoyMDQ1MzM3MzMsImxpZCI6InZKNm5rcGZTQjlpUG9tbzg0NnpGa1EifSwiaXNzIjoiaHR0cHM6Ly9hdXRoLnJpb3RnYW1lcy5jb20iLCJleHAiOjE2NzgzMzY4MDUsImlhdCI6MTY3ODMzMzIwNSwianRpIjoicVNBd09HWWh3dzQiLCJjaWQiOiJwbGF5LXZhbG9yYW50LXdlYi1wcm9kIn0.UcFYmsC7UlfImmuYNw1NP146RB9v6SKWtc-i6Hs1E3ehcdjDmeqInCKeK55fZviwFaB6Cky-ykBldTBHRtROZiCjNOSkiok3VdYVjXcRjswupMTLmtnewERR4hCYYQVaFe9HIumB1xZJzzoRs5lq37ugpmGrY0x0HcNGwAgj908",
     },
-    body: '{"type":"auth","username":"killjoy2305","password":"zarzal1169","remember":true,"language":"en_US"}',
   };
 
   fetch(url, options)
     .then((res) => res.json())
-    .then((json) => {})
+    .then((json) => console.log(json))
     .catch((err) => console.error("error:" + err));
 }
 
@@ -157,7 +382,7 @@ let state = "Loading...";
 let globalSocket = null;
 
 async function run() {
-  let lockData = null;
+  lockData = null;
   do {
     try {
       lockData = await getLockfileData();
@@ -213,13 +438,13 @@ async function run() {
   try {
     await fs.promises.mkdir("./logs");
   } catch (ignored) {}
-  const logPath = `./logs/${new Date().getTime()}.txt`;
-  console.log(`Writing to ${logPath}`);
+  // const logPath = `./logs/${new Date().getTime()}.txt`;
+  // console.log(`Writing to ${logPath}`);
 
-  const logStream = fs.createWriteStream(logPath);
-  logStream.write(JSON.stringify(lockData) + "\n");
-  logStream.write(JSON.stringify(sessionData) + "\n");
-  logStream.write(JSON.stringify(helpData) + "\n\n");
+  // const logStream = fs.createWriteStream(logPath);
+  // logStream.write(JSON.stringify(lockData) + "\n");
+  // logStream.write(JSON.stringify(sessionData) + "\n");
+  // logStream.write(JSON.stringify(helpData) + "\n\n");
 
   const ws = new WebSocket(
     `wss://riot:${lockData.password}@127.0.0.1:${lockData.port}`,
@@ -228,7 +453,7 @@ async function run() {
     }
   );
 
-  ws.on("open", () => {
+  ws.on("open", async () => {
     Object.entries(helpData.events).forEach(([name, desc]) => {
       if (name === "OnJsonApiEvent") return;
       ws.send(JSON.stringify([5, name]));
@@ -236,39 +461,105 @@ async function run() {
     state = "Connected to websocket!";
     globalSocket?.emit("console", state);
     console.log("Connected to websocket!");
+    globalSocket?.emit("connected");
+
+    await getVersion();
+    await getEntitlementsToken(lockData.port, lockData.password);
+    await getPUUID(lockData.port, lockData.password);
+    await getPartyPlayer();
+    await getParty();
   });
 
-  ws.on("message", (data) => {
-    logStream.write(new Date().getTime() + " " + data + "\n");
-    globalSocket?.emit("message", data);
+  ws.on("message", async (data) => {
+    // logStream.write(new Date().getTime() + " " + data + "\n");
+
+    const dataString =
+      data.toString().length > 0 ? JSON.parse(data?.toString()) : null;
+    if (dataString === null) return;
+
+    const eventType = dataString[0];
+    const eventName = dataString[1];
+    const event = dataString[2];
+
+    // console.log(eventName, event);
+
+    if (eventName === "OnJsonApiEvent_riot-messaging-service_v1_message") {
+      if (event.data.service === "pregame") {
+        // console.log(event);
+        if (
+          event.uri.includes(
+            "/riot-messaging-service/v1/message/ares-pregame/pregame/v1/matches/"
+          )
+        ) {
+          await getPregame();
+          globalSocket?.emit("preGameEvent", preGame);
+        }
+      }
+    }
+
+    // if (eventName === "OnJsonApiEvent_chat_v4_presences") {
+    //   await getParty();
+    //   globalSocket?.emit("updateData");
+    // }
   });
 
   ws.on("close", () => {
     state = "Websocket closed!";
     globalSocket?.emit("console", state);
     console.log("Websocket closed!");
-    logStream.end();
+    globalSocket?.emit("disconnected");
+    // logStream.end();
   });
-
-  // getEntitlementsToken(lockData.port);
 }
 
-// run();
-authCookies();
+run();
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   globalSocket = socket;
-
   console.log("Usuario conectado");
+  socket.emit("connected");
 
   socket.emit("console", state);
 
-  socket.on("mensaje", (data) => {
-    console.log(`Mensaje recibido: ${data}`);
-    io.emit("mensaje", data);
+  puuid && socket.emit("puuid", puuid);
+
+  gameModes && socket.emit("gamemodes", gameModes);
+
+  party && socket.emit("party", party);
+
+  socket.on("updateData", () => {
+    puuid && socket.emit("puuid", puuid);
+    gameModes && socket.emit("gamemodes", gameModes);
+    party && socket.emit("party", party);
+  });
+
+  socket.on("setGamemode", async (data) => {
+    console.log("setGamemode", data);
+    await changeQueue(data);
+  });
+
+  socket.on("startQueue", async () => {
+    console.log("startQueue");
+    await startQueue();
+  });
+
+  socket.on("stopQueue", async () => {
+    console.log("stopQueue");
+    await stopQueue();
+  });
+
+  socket.on("partyAccess", async (data) => {
+    console.log("partyAccess", data);
+    await partyAccessibility(data);
+  });
+
+  socket.on("selectAgent", async (data) => {
+    console.log("selectAgent", data);
+    await selectAgent(data);
   });
 
   socket.on("disconnect", () => {
+    socket.emit("console", "Disconnected");
     console.log("Usuario desconectado");
   });
 });
